a87306570858214482bfeb06c0e5db1a5a1ff59d
Angelo Cavalcanti <angelo.cavalcanti@hotmail.com>
2023-04-21 04:29:12 -0300

    Item 4. Adicionar contas.

--- app/src/main/java/br/ufpe/cin/residencia/banco/conta/AdicionarContaActivity.java
+++ app/src/main/java/br/ufpe/cin/residencia/banco/conta/AdicionarContaActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import br.ufpe.cin.residencia.banco.R;
 
@@ -38,14 +39,74 @@ public class AdicionarContaActivity extends AppCompatActivity {
                     String numeroConta = campoNumero.getText().toString();
                     String saldoConta = campoSaldo.getText().toString();
                     //TODO: Incluir validações aqui, antes de criar um objeto Conta (por exemplo, verificar que digitou um nome com pelo menos 5 caracteres, que o campo de saldo tem de fato um número, assim por diante). Se todas as validações passarem, aí sim cria a Conta conforme linha abaixo.
-                    Conta c = new Conta(numeroConta, Double.valueOf(saldoConta), nomeCliente, cpfCliente);
-                    //TODO: chamar o método que vai salvar a conta no Banco de Dados
+
                     // ANGELO ABAIXO
-                    viewModel.inserir(c);
-                    finish();
+                    String msg="";
+                    if(nomeCliente != null && nomeCliente.length() >= 5) {
+                        if (validarCPF(cpfCliente)) {
+                            if(!numeroConta.isEmpty() && numeroConta != null) {
+                                if(!saldoConta.isEmpty() && saldoConta != null && saldoConta.matches(
+                                        "[0-9]+")) {
+                                    Conta c = new Conta(numeroConta, Double.valueOf(saldoConta),
+                                            nomeCliente, cpfCliente);
+                                    viewModel.inserir(c);
+                                    finish();
+                                }else{
+                                    msg="Digite um saldo com números.";
+                                }
+                            }else {
+                                msg = "Digite um número da conta.";
+                            }
+                        }else {
+                            msg = "Digite um CPF válido.";
+                        }
+                    }else {
+                        msg = "Digite um nome com no mínimo 5 caracteres.";
+                    }
+                    if (!msg.equals("")) {
+                        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+                    }
                     // ANGELO ACIMA
+                    //TODO: chamar o método que vai salvar a conta no Banco de Dados
                 }
         );
 
+    }
+    public static boolean validarCPF(String cpf) {
+        cpf = cpf.replace(".","").replace("-","").trim();
+        if (cpf == null || cpf.length() != 11)
+            return false;
+
+        try {
+            Long.parseLong(cpf);
+        } catch (NumberFormatException e) { // CPF não possui somente números
+            return false;
+        }
+
+        return calcDigVerif(cpf.substring(0, 9)).equals(cpf.substring(9, 11));
+    }
+    private static String calcDigVerif(String num) {
+        Integer primDig, segDig;
+        int soma = 0, peso = 10;
+        for (int i = 0; i < num.length(); i++)
+            soma += Integer.parseInt(num.substring(i, i + 1)) * peso--;
+
+        if (soma % 11 == 0 | soma % 11 == 1)
+            primDig = new Integer(0);
+        else
+            primDig = new Integer(11 - (soma % 11));
+
+        soma = 0;
+        peso = 11;
+        for (int i = 0; i < num.length(); i++)
+            soma += Integer.parseInt(num.substring(i, i + 1)) * peso--;
+
+        soma += primDig.intValue() * 2;
+        if (soma % 11 == 0 | soma % 11 == 1)
+            segDig = new Integer(0);
+        else
+            segDig = new Integer(11 - (soma % 11));
+
+        return primDig.toString() + segDig.toString();
     }
 }
